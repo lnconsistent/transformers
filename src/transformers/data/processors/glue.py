776f94d4d0fefaa9f5ac08687231a2f738646b69
lnconsistent <yx1797@nyu.edu>
2020-03-11 19:48:57 -0400

    Update glue.py

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -515,6 +515,41 @@ class WnliProcessor(DataProcessor):
             examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
 
+    
+class BoolqProcessor(DataProcessor):
+    
+    def get_example_from_tensor_dict(self, tensor_dict):
+    """See base class."""
+    return InputExample(
+        tensor_dict["idx"].numpy(),
+        tensor_dict["passage"].numpy().decode("utf-8"),
+        tensor_dict["question"].numpy().decode("utf-8"),
+        str(tensor_dict["label"].numpy()),
+    )
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev_matched")
+
+    def get_labels(self):
+        """See base class."""
+        return ["TRUE", "FALSE"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, line[0])
+            text_a = line[1]
+            text_b = line[2]
+            label = line[-1]
+            examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+        return examples
 
 glue_tasks_num_labels = {
     "cola": 2,
